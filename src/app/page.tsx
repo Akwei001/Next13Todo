5a30f5b295add987ccd937a049d53edc19380d22
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-07-20 20:47:08 +0100

    header styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <header>
+      <header className='flex items-center justify-between mb-4'>
         <h1 className='text-2xl'>Todos</h1>
         <Link href='/new'> New </Link>
         <ul></ul>
