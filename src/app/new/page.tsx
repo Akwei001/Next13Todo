ce2ea21b2abb89f350c12f4bb574b2ee5f49e6df
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2023-07-23 16:26:31 +0100

    complete

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,47 @@
-export default function Pome() {
+import { prisma } from '@/db';
+import { redirect } from 'next/navigation';
+import Link from 'next/link';
+
+async function createTodo(data: FormData) {
+  'use server';
+
+  const title = data.get('title')?.valueOf();
+  if (typeof title !== 'string' || title.length === 0) {
+    throw new Error('Invalid title');
+  }
+
+  await prisma.todo.create({ data: { title, complete: false } });
+
+  redirect('/');
+}
+
+export default function Page() {
   return (
     <>
-      <h1>New</h1>
+      <header className='flex items-center justify-between mb-4'>
+        <h1 className='text-2xl'>New</h1>
+      </header>
+      <form action={createTodo} className='flex flex-col gap-2'>
+        <input
+          type='text'
+          name='title'
+          className='px-2 py-1 bg-transparent border rounded outline-none border-slate-300 focus-within:border-slate-100'
+        />
+        <div className='flex justify-end gap-1'>
+          <Link
+            href='..'
+            className='px-2 py-1 border rounded outline-none border-slate-300 text-slate-300 hover:bg-slate-700 focus-within:bg-slate-700'
+          >
+            Cancel
+          </Link>
+          <button
+            type='submit'
+            className='px-2 py-1 border rounded outline-none border-slate-300 text-slate-300 hover:bg-slate-700 focus-within:bg-slate-700'
+          >
+            Create
+          </button>
+        </div>
+      </form>
     </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
 import Link from 'next/link';
+import { prisma } from '@/db';
+import { TodoItem } from '@/components/TodoItem';
 
-export default function Home() {
+function getTodos() {
+  return prisma.todo.findMany();
+}
+
+async function toggleTodo(id: string, complete: boolean) {
+  'use server';
+
+  console.log(id, complete);
+
+  await prisma.todo.update({ where: { id }, data: { complete } });
+}
+
+export default async function Home() {
+  const todos = await getTodos();
+
   return (
     <>
       <header className='flex items-center justify-between mb-4'>
         <h1 className='text-2xl'>Todos</h1>
-        <Link href='/new'> New </Link>
-        <ul>A list item</ul>
+        <Link
+          className='px-2 py-1 border rounded outline-none border-slate-300 hover: bg-slate-700 focus-within:bg-slate-700'
+          href='/new'
+        >
+          New
+        </Link>
       </header>
+      <ul className='pl-4'>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+        ))}
+      </ul>
     </>
   );
 }
